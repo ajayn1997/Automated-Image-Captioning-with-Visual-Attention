68a7f129b838ed7ed56da0c13e71af76977cb5c4
Ajay Narayanan <the.ajay.great@gmail.com>
2020-05-22 15:02:38 +0530

    Commit

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -5,11 +5,13 @@ import numpy as np
 import os
 import json
 
-def get_train_data(num_examples=3000):
+def get_train_data(num_examples=30000):
+    
     annotation_zip = tf.keras.utils.get_file('captions.zip',
                                              cache_subdir=os.path.abspath('.'),
                                              origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
                                              extract = True)
+    
     annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
 
     name_of_zip = 'train2014.zip'
@@ -101,8 +103,7 @@ def text_to_vec(train_captions):
     return  tokenizer,cap_vector
 
 
-
-
+get_train_data()
 
 
     
